9bdf0c080fab41c60e875bd86494d02a1a5a8d36
StefanCardnell <raging_rush@hotmail.co.uk>
2020-06-27 15:45:21 +0100

    Caching 50MB of images

--- app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.java
@@ -9,6 +9,7 @@ import android.os.Message;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.collection.LruCache;
 
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,18 +21,29 @@ public class ThumbnailDownloader<T> extends HandlerThread {
 
     private static final int MESSAGE_DOWNLOAD = 0;
 
+    private static final int LRU_CACHE_SIZE = 50 * 1024 * 1024; // 50MB cache
+
     private boolean mHasQuit = false;
     private Handler mRequestHandler;
+    // mRequestMap will hold the URL that currently needs to be fetched for a target T. It needs be
+    // kept up to date and checked as Messages are created and processed, to ensure the currently
+    // desired URL for a target is fetched.
     private ConcurrentMap<T, String> mRequestMap = new ConcurrentHashMap<>();
     private Handler mResponseHandler;
-    private ThumbnailDownloadListener<T> mTThumbnailDownloadListener;
+    private ThumbnailDownloadListener<T> mThumbnailDownloadListener;
+    private LruCache<String, Bitmap> mLruCache = new LruCache<String, Bitmap>(LRU_CACHE_SIZE){
+        @Override
+        protected int sizeOf(@NonNull String key, @NonNull Bitmap value) {
+            return value.getByteCount();
+        }
+    };
 
     public interface ThumbnailDownloadListener<T> {
         void onThumbnailDownloaded(T target, Bitmap thumbnail);
     }
 
     public void setThumbnailDownloadListener(ThumbnailDownloadListener<T> listener) {
-        mTThumbnailDownloadListener = listener;
+        mThumbnailDownloadListener = listener;
     }
 
     public ThumbnailDownloader(Handler responseHandler){
@@ -65,7 +77,11 @@ public class ThumbnailDownloader<T> extends HandlerThread {
 
         if(url == null){
             mRequestMap.remove(target);
-        } else {
+        } else if (mLruCache.get(url) != null) {
+            Bitmap cachedBitmap = mLruCache.get(url);
+            mThumbnailDownloadListener.onThumbnailDownloaded(target, cachedBitmap);
+        }
+        else {
             mRequestMap.put(target, url);
             mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget();
         }
@@ -83,16 +99,25 @@ public class ThumbnailDownloader<T> extends HandlerThread {
 
             if (url == null) return;
 
-            byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
-            final Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
-            Log.i(TAG, "Bitmap created");
+            final Bitmap bitmap;
+            // A different message could've fetched the URL in the time between queueThumbnail and
+            // handleRequest executing for this message.
+            if(mLruCache.get(url) != null){
+                bitmap = mLruCache.get(url);
+            } else {
+                byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
+                bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+                mLruCache.put(url, bitmap); // Cache for later
+                int cacheUsage = (int) (100 * ((float) mLruCache.size() / LRU_CACHE_SIZE));
+                Log.i(TAG, "Bitmap created and cached. Cached Images: " + cacheUsage + "%");
+            }
 
             mResponseHandler.post(new Runnable() {
                 public void run() {
                     if( mRequestMap.get(target) != url || mHasQuit) return;
 
                     mRequestMap.remove(target);
-                    mTThumbnailDownloadListener.onThumbnailDownloaded(target, bitmap);
+                    mThumbnailDownloadListener.onThumbnailDownloaded(target, bitmap);
                 }
             });
 
